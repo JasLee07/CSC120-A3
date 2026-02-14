40f378349f7854ed019b5cbd1f5d7c97683c32b1
JasLee07 <jrlee@smith.edu>
2026-02-14 17:03:19 -0500

    Conversation function, added rounds?

--- Conversation.java
+++ Conversation.java
@@ -1,16 +1,22 @@
 // You should **not** update any call signatures in this file
 // only modify the body of each function
+import java.util.Scanner;
 class Conversation implements ConversationRequirements {
 
   // Attributes 
 
+
+  
   /**
    * Constructor 
    */
   Conversation() {
-    
-  }
-
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("How many rounds?");
+    int rounds = scanner.nextInt();
+    System.out.println("You chose " + rounds + " rounds");
+    }
+  
   /**
    * Starts and runs the conversation with the user
    */
